866555536bb5926a5406f35945952a35e583d53e
T0fuHasuu <dayuththy@gmail.com>
2025-09-17 01:30:58 +0700

    Test Commit

--- passGenerator/main.py
+++ passGenerator/main.py
@@ -1,1 +1,10 @@
 # Password generator 
+def checkPass(passwd):
+    if passwd is None:
+        return "Check Again"
+    elif len(passwd) < 8:
+        return "Too Short"
+    elif len(passwd) == 8:
+        return passwd
+
+print(checkPass("12345678"))
